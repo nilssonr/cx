730e00b66e87ee3c0abd4b2cb679c57a2731c837
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2024-11-25 06:24:28 -0700

    fixed uuid namespace

--- src/cx_uuid.erl
+++ src/cx_uuid.erl
@@ -2,10 +2,10 @@
 
 -export([v1/0, v2/0, v3/1, v4/0, v5/0, v6/0, v7/0, v8/0]).
 
--define(UUID_NAMESPACE_DNS, <<16#6ba7b8109dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_URL, <<16#6ba7b8119dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_OID, <<16#6ba7b8129dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_X500, <<16#6ba7b8149dad11d180b400c04fd430c8:128>>).
+-define(NAMESPACE_DNS, <<16#6ba7b8109dad11d180b400c04fd430c8:128>>).
+-define(NAMESPACE_URL, <<16#6ba7b8119dad11d180b400c04fd430c8:128>>).
+-define(NAMESPACE_OID, <<16#6ba7b8129dad11d180b400c04fd430c8:128>>).
+-define(NAMESPACE_X500, <<16#6ba7b8149dad11d180b400c04fd430c8:128>>).
 
 %%%-------------------------------------------------------------------
 %%% Public API
